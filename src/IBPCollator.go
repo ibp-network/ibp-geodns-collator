f2fad4084d2ef901603bb6844ee9d9a4af99d732
Tom <tsenseless@gmail.com>
2025-11-02 08:59:12 -0500

    Update

--- src/IBPCollator.go
+++ src/IBPCollator.go
@@ -19,6 +19,8 @@ import (
 
 var version = cfg.GetVersion()
 
+const checkTypeNormalizeInterval = 5 * time.Second
+
 func main() {
 	log.Log(log.Info, "IBPCollator v%s starting …", version)
 
@@ -79,4 +81,45 @@ func main() {
 	for {
 		time.Sleep(1 * time.Hour)
 	}
+}
+
+func startMemberEventCheckTypeNormalizer() {
+	go func() {
+		ticker := time.NewTicker(checkTypeNormalizeInterval)
+		defer ticker.Stop()
+
+		for {
+			if err := normalizeMemberEventCheckTypes(); err != nil {
+				log.Log(log.Error, "[collator] normalize member_events check_type: %v", err)
+			}
+
+			<-ticker.C
+		}
+	}()
+}
+
+func normalizeMemberEventCheckTypes() error {
+	if data2.DB == nil {
+		return nil
+	}
+
+	res, err := data2.DB.Exec(`
+		UPDATE member_events
+		SET check_type = CASE check_type
+			WHEN '1' THEN 'site'
+			WHEN '2' THEN 'domain'
+			WHEN '3' THEN 'endpoint'
+			ELSE check_type
+		END
+		WHERE check_type IN ('1','2','3')
+	`)
+	if err != nil {
+		return err
+	}
+
+	if rows, err := res.RowsAffected(); err == nil && rows > 0 {
+		log.Log(log.Debug, "[collator] normalized %d member_events check_type row(s)", rows)
+	}
+
+	return nil
 }
