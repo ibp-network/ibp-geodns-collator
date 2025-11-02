7e6cb998a4e30feef54c36cf123937c9782c2a75
Tom <tsenseless@gmail.com>
2025-11-02 09:44:21 -0500

    Update

--- src/IBPCollator.go
+++ src/IBPCollator.go
@@ -78,3 +78,29 @@ func main() {
 		time.Sleep(1 * time.Hour)
 	}
 }
+
+func migrateMemberEventCheckTypesToNumeric() error {
+	if data2.DB == nil {
+		return nil
+	}
+
+	res, err := data2.DB.Exec(`
+		UPDATE member_events
+		SET check_type = CASE check_type
+			WHEN 'site' THEN '1'
+			WHEN 'domain' THEN '2'
+			WHEN 'endpoint' THEN '3'
+			ELSE check_type
+		END
+		WHERE check_type IN ('site','domain','endpoint')
+	`)
+	if err != nil {
+		return err
+	}
+
+	if rows, err := res.RowsAffected(); err == nil && rows > 0 {
+		log.Log(log.Debug, "[collator] migrated %d member_events check_type row(s) to numeric form", rows)
+	}
+
+	return nil
+}
